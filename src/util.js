1a4965d1686d4713533b044df7a3f8c90fbc800e
tabku <41511147+tabku@users.noreply.github.com>
2022-06-30 08:03:49 +0900

    map filter

--- src/util.js
+++ src/util.js
@@ -28,9 +28,32 @@
 
 // 配列のコピー、結合
 // 参照渡しにならないので、コピー結合はスプレッド構文でやるのが吉
-const arr3 = [10, 20, 30];
-const arr5 = [10, 20, 30];
-const arr4 = [...arr3];
-console.log(arr4);
+// const arr3 = [10, 20, 30];
+// const arr5 = [10, 20, 30];
+// const arr4 = [...arr3];
+// console.log(arr4);
 
-console.log([...arr3, ...arr5]);
+// console.log([...arr3, ...arr5]);
+
+// map filterを使った配列の処理
+// const nameArr = ["tom", "soge", "hoge"];
+// // const nameArr2 = nameArr.map((name) => name);
+// // console.log(nameArr2);
+// const nameArr2 = nameArr.map((name) => {
+//   return `${name}さん`;
+// });
+// console.log(nameArr2);
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => num % 2 === 0);
+// console.log(newNumArr);
+
+// || 左側がfalseなら右を実行する
+// const num = 100;
+// const fee = num || "金額未設定";
+// console.log(fee);
+
+// && 左側がtrueなら右を実行する
+const test = true;
+const test2 = 1;
+console.log(test && test2);
